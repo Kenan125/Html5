37ba2db81ae0d74d3e09ddea6d14f5bfa6ea69c9
Kanan Ismayilzada <kenanismayilzade12@gmail.com>
2024-07-09 22:06:52 +0300

    "Added jQuery code for HTML DOM manipulation and events, including text changes, hide/show effects, and mouseenter/mouseleave events."

--- Jquery/Jquery-Html-DOM.js
+++ Jquery/Jquery-Html-DOM.js
@@ -6,7 +6,46 @@
 //jquery.com veya cdnjs.com sayfalarindan
 
 
-$.ad = "Ali";
+$.ad = "Ali";   //this gorur
 $.soyad = "Uçar";
 
-console.log($.ad+" "+ $.soyad)
+$.product = {
+    name: "televizyon",
+    price: 22000
+}
+
+$.sonuc = $.product.name
+
+
+//javascript komutlarinin html sayfasi yuklendikten sonra calismasi icin document.onload = alert("Sayfa yuklendi.")
+//Jquery komutlarinin html sayfasi yuklendikten sonra calismasi icin;
+/* $(document).ready(function(){
+    alert("Sayfa yuklendi.")
+}) */
+
+//Jquery events (olaylar)
+//click. show, hide, mouseenter, mouseleave, dbclick gibi
+
+$(document).ready(function(){
+    $('#text').text('Sayfa yuklendi.') //id'si text olan paragrafin yazisini degistirir.
+    $('#text').html('<em>Sayfa yuklendi.</em>') //id'si text olan paragraf
+    $('#degistir').click(function(){
+        $('#text').hide(2000);
+    })
+
+})
+$(document).ready(function(){
+    $('#sonuc').mouseenter(function(){
+        $(this).css('background', 'yellow');
+    })
+    $('#sonuc').mouseleave(function(){
+
+        $(this).css('background', 'red');
+})
+})
+
+
+$(document).ready(function(){
+    $('goster')
+})
+console.log($.sonuc)
